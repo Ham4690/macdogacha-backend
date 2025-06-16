f7e4a810ed16b1d198bb890bd65ae0bb93e221e5
Arata Higashiguchi <ham46903@gmail.com>
2025-06-16 20:50:47 +0900

    setup base spring pj

---
+++ src/main/java/com/macdogacha/backend/MacdogachaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.macdogacha.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacdogachaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacdogachaBackendApplication.class, args);
+	}
+
+}
